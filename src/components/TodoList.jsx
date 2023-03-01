962b059f9a114eafc04880f3111c192aafcd677b
himelhasan <himelhasan497@gmail.com>
2023-03-01 10:25:44 +0600

    filtering working for todo data

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import store from "../Redux/store";
 import TaskSingle from "./TaskSingle/TaskSingle";
 
 const TodoList = () => {
-  const allTodos = useSelector((state) => state.todo);
-  console.log(allTodos);
+  const allTodo = useSelector((state) => state.todo);
+  const allFilter = useSelector((state) => state.filterReducer);
+  const { colors, status } = allFilter;
+
+  const filterByStatus = (todo) => {
+    switch (status) {
+      case "Complete":
+        return todo.completed;
+      case "Incomplete":
+        return !todo.completed;
+      default:
+        return true;
+    }
+  };
+
+  const filterByColor = (todo) => {
+    if (colors.length > 0) {
+      return colors?.includes(todo.color);
+    }
+    return true;
+  };
+
   return (
     <div className="mt-2 text-gray-700 text-sm max-h-[300px] overflow-y-auto">
-      {allTodos.map((todo) => (
-        <TaskSingle key={todo.id} todo={todo}></TaskSingle>
-      ))}
+      {allTodo
+        .filter(filterByStatus)
+        .filter(filterByColor)
+        .map((todo) => (
+          <TaskSingle key={todo.id} todo={todo}></TaskSingle>
+        ))}
     </div>
   );
 };
